21bf12b1b16d15d40bcddcaae7f5da5639a43d83
oduortoni <oduortoni@gmail.com>
2025-03-06 12:25:03 +0300

    fix: minimize content type radius
    ensure we set header type per conditional branch
    this reduces the chances of trying to reset headers and leading to superflous header errors

--- handlers/middleware/auth.go
+++ handlers/middleware/auth.go
@@ -10,24 +10,23 @@ import (
 )
 
 type Response struct {
-	Code int
-	Message string
+	Code     int
+	Message  string
 	Redirect string
 }
 
 // AuthMiddleware checks if the user has a valid session and attaches the user information to the request context.
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
 		// If we have a valid session, fetch session and user info
 
 		session, loggedIn := database.IsLoggedIn(r)
 		if !loggedIn {
+			w.Header().Set("Content-Type", "application/json")
 			// http.Redirect(w, r, "/login", http.StatusSeeOther)
 			r := Response{
-				Code: http.StatusTemporaryRedirect,
-				Message: "User is not logged in. Please log in to try again",
+				Code:     http.StatusTemporaryRedirect,
+				Message:  "User is not logged in. Please log in to try again",
 				Redirect: "/login",
 			}
 			json.NewEncoder(w).Encode(r)
@@ -36,12 +35,13 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 		// Check if session is expired
 		if time.Now().After(session.Expiry) {
+			w.Header().Set("Content-Type", "application/json")
 			// Session expired, delete it and return Unauthorized
 			_ = database.DeleteSession(session.SessionID)
 			// http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			r := Response{
-				Code: http.StatusUnauthorized,
-				Message: "Session expired. Please log in to try again",
+				Code:     http.StatusUnauthorized,
+				Message:  "Session expired. Please log in to try again",
 				Redirect: "/login",
 			}
 			json.NewEncoder(w).Encode(r)

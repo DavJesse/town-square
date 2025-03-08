b4adc2ba8b9abd086fd0ccc5d71bdcc42f9f4c58
oduortoni <oduortoni@gmail.com>
2025-03-08 06:51:54 +0300

    fix: remove the window.onload from index.js

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,13 +1,3 @@
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
 }
-
-// window.onload = render;
-
-// function render() {
-//     fetch("/")
-//     .then(data => data.json())
-//     .then(data => {
-//         console.log(data);
-//     })
-// }

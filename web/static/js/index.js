e703cfc2ec1585b74752078480f7b4a22dcfceca
oduortoni <oduortoni@gmail.com>
2025-03-08 06:48:32 +0300

    feat: can get posts by category without a page reload
    on load, spa.html includes the category buttons that hava an onclick listener
    when yu click to view the category, you do not need a page refresh, instead, a javascript fetch happens and updates the page

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,3 +1,13 @@
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
 }
+
+// window.onload = render;
+
+// function render() {
+//     fetch("/")
+//     .then(data => data.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+// }

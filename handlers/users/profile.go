9f9829f9d3afd85a65b221142df1dc93b52c0a82
davodhiambo <jesseomolo@yahoo.com>
2025-03-24 18:16:07 +0300

    change primary color to a richer shade of blue.

--- handlers/users/profile.go
+++ handlers/users/profile.go
@@ -11,53 +11,53 @@ import (
 
 // ViewUserProfile handler
 func ViewUserProfile(w http.ResponseWriter, r *http.Request) {
-    if r.Header.Get("Accept") == "application/json" {
-        serveUserProfileJSON(w, r)
-        return
-    }
+	if r.Header.Get("Accept") == "application/json" {
+		serveUserProfileJSON(w, r)
+		return
+	}
 
-    // Serve index.html for frontend SPA routing
-    http.ServeFile(w, r, "./web/templates/index.html")
+	// Serve index.html for frontend SPA routing
+	http.ServeFile(w, r, "./web/templates/index.html")
 }
 
 func serveUserProfileJSON(w http.ResponseWriter, r *http.Request) {
-    session, logged := database.IsLoggedIn(r)
-    if !logged {
-        http.Redirect(w, r, "/login", http.StatusSeeOther)
-        return
-    }
+	session, logged := database.IsLoggedIn(r)
+	if !logged {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
 
-    userData, err := database.GetUserbySessionID(session.SessionID)
-    if err != nil {
-        log.Printf("Error getting user: %v\n", err)
-        w.Header().Set("Content-Type", "application/json")
-        json.NewEncoder(w).Encode(models.Response{
-            Code:     http.StatusTemporaryRedirect,
-            Message:  "Error getting user",
-            Redirect: "/login",
-        })
-        return
-    }
+	userData, err := database.GetUserbySessionID(session.SessionID)
+	if err != nil {
+		log.Printf("Error getting user: %v\n", err)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(models.Response{
+			Code:     http.StatusTemporaryRedirect,
+			Message:  "Error getting user",
+			Redirect: "/login",
+		})
+		return
+	}
 
-    UserPosts, err := database.PostsFilterByUser(userData.ID)
-    if err != nil {
-        log.Printf("Error getting posts: %v\n", err)
-    }
+	UserPosts, err := database.PostsFilterByUser(userData.ID)
+	if err != nil {
+		log.Printf("Error getting posts: %v\n", err)
+	}
 
-    profileData := struct {
-        User  models.User
-        Posts []models.Post
-    }{
-        User:  userData,
-        Posts: UserPosts,
-    }
+	profileData := struct {
+		User  models.User
+		Posts []models.Post
+	}{
+		User:  userData,
+		Posts: UserPosts,
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(models.Response{
-        Code:    http.StatusOK,
-        Message: "Profile data retrieved",
-        Data:    profileData,
-    })
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(models.Response{
+		Code:    http.StatusOK,
+		Message: "Profile data retrieved",
+		Data:    profileData,
+	})
 }
 
 // func ViewUserProfile(w http.ResponseWriter, r *http.Request) {

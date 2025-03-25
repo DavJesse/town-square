b6ded2f0d65ea9b3b501543a864a3ed045e9f54a
davodhiambo <jesseomolo@yahoo.com>
2025-03-25 12:29:24 +0300

    refactor code to remove unused code.

--- handlers/users/profile.go
+++ handlers/users/profile.go
@@ -11,109 +11,40 @@ import (
 
 // ViewUserProfile handler
 func ViewUserProfile(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Accept") == "application/json" {
-		serveUserProfileJSON(w, r)
-		return
-	}
-
-	// Serve index.html for frontend SPA routing
-	http.ServeFile(w, r, "./web/templates/index.html")
-}
-
-func serveUserProfileJSON(w http.ResponseWriter, r *http.Request) {
 	session, logged := database.IsLoggedIn(r)
 	if !logged {
-		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
 
 	userData, err := database.GetUserbySessionID(session.SessionID)
 	if err != nil {
 		log.Printf("Error getting user: %v\n", err)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(models.Response{
-			Code:     http.StatusTemporaryRedirect,
-			Message:  "Error getting user",
-			Redirect: "/login",
-		})
+		http.Error(w, "Error retrieving user data", http.StatusInternalServerError)
 		return
 	}
 
-	UserPosts, err := database.PostsFilterByUser(userData.ID)
+	userPosts, err := database.PostsFilterByUser(userData.ID)
 	if err != nil {
 		log.Printf("Error getting posts: %v\n", err)
+		// Continue execution but return empty posts if an error occurs
+		userPosts = []models.Post{}
 	}
 
+	// Combine user data and user posts
 	profileData := struct {
-		User  models.User
-		Posts []models.Post
+		User  models.User   `json:"user"`
+		Posts []models.Post `json:"posts"`
 	}{
 		User:  userData,
-		Posts: UserPosts,
+		Posts: userPosts,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(models.Response{
 		Code:    http.StatusOK,
 		Message: "Profile data retrieved",
 		Data:    profileData,
 	})
 }
-
-// func ViewUserProfile(w http.ResponseWriter, r *http.Request) {
-// 	session, logged := database.IsLoggedIn(r)
-// 	if !logged {
-// 		http.Redirect(w, r, "/login", http.StatusSeeOther)
-// 		return
-// 	}
-
-// 	userData, err := database.GetUserbySessionID(session.SessionID)
-// 	// fmt.Printf("UserData retrieved: %+v\n", userData)  // Add debug logging
-// 	if err != nil {
-// 		log.Printf("Error getting user: %v\n", err) // Add error logging
-// 		w.Header().Set("Content-Type", "application/json")
-// 		r := models.Response{
-// 			Code:     http.StatusTemporaryRedirect,
-// 			Message:  "Error getting user",
-// 			Redirect: "/login",
-// 		}
-// 		json.NewEncoder(w).Encode(r)
-// 		// http.Redirect(w, r, "/login", http.StatusSeeOther)
-// 		return
-// 	}
-
-// 	UserPosts, err := database.PostsFilterByUser(userData.ID)
-// 	if err != nil {
-// 		log.Printf("Error getting posts: %v\n", err) // Add error logging
-// 		// w.Header().Set("Content-Type", "application/json")
-// 		// r := models.Response{
-// 		// 	Code:     http.StatusOK,
-// 		// 	Message:  "Error getting posts",
-// 		// 	Redirect: "/login",
-// 		// }
-// 		// json.NewEncoder(w).Encode(r)
-// 		// return
-// 	}
-
-// 	// Combine user data and user posts into a single struct
-// 	profileData := struct {
-// 		User  models.User
-// 		Posts []models.Post
-// 	}{
-// 		User:  userData,
-// 		Posts: UserPosts,
-// 	}
-// 	// Debug logs
-// 	// fmt.Println(profileData.User)
-// 	// fmt.Println("-------")
-// 	// fmt.Println(profileData.Posts)
-
-// 	w.Header().Set("Content-Type", "application/json")
-// 	req := models.Response{
-// 		Code:     http.StatusOK,
-// 		Message:  "Error getting posts",
-// 		Redirect: "",
-// 		Data:     profileData,
-// 	}
-// 	json.NewEncoder(w).Encode(req)
-// }

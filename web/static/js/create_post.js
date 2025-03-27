c7f8cb18a917b73a4227f31593f026277eb95160
davodhiambo <jesseomolo@yahoo.com>
2025-03-27 20:48:03 +0300

    feat: add createPostButton logic and dependant functions.

---
+++ web/static/js/create_post.js
@@ -0,0 +1,24 @@
+export function createPostButton() {
+    // Create button element
+    let createPostButton = document.createElement('button');
+    createPostButton.id = 'create_post_button';
+
+    // Create plus sign element
+    let plusSign = document.createElement('span');
+    plusSign.id = 'plus_sign';
+    plusSign.textContent = '+';
+    createPostButton.appendChild(plusSign);
+
+    // Append button to app
+    let app = document.getElementById('app');
+    app.appendChild(createPostButton);
+
+    return createPostButton
+}
+
+export function setCreatePostsButtonListeners(createPostButton) {
+    // Add Event listener for clicking create posts button
+    createPostButton.addEventListener('click', () => {
+        alert('Create post button clicked');
+    });
+}

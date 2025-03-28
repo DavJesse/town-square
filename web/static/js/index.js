f5be2e8aefb2fa7efc7584009f9ebf17264e8baf
davodhiambo <jesseomolo@yahoo.com>
2025-03-28 14:06:20 +0300

    layout: append index page components to dom.

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -33,9 +33,9 @@ export function renderIndexPage() {
     centerCluster.id = 'center_cluster';
     rightCluster.id = 'right_cluster';
 
-    indexPageContainer.appendChild(rightCluster);
-    indexPageContainer.appendChild(centerCluster);
     indexPageContainer.appendChild(leftCluster);
+    indexPageContainer.appendChild(centerCluster);
+    indexPageContainer.appendChild(rightCluster);
 
     // Create categories and online users containers
     let categoriesCard = document.createElement('div');
@@ -49,8 +49,75 @@ export function renderIndexPage() {
     onlineUsersCard.appendChild(onlineUsersTitle);
     leftCluster.appendChild(categoriesCard);
     leftCluster.appendChild(onlineUsersCard);
-    indexPageContainer.appendChild(leftCluster);
+   
+    // Create container for post toggling button
+    let postsButtonContainer = document.createElement('div');
+    postsButtonContainer.id = 'posts_button_container';
 
+    // Create buttons to toggle prefered posts    
+    let myPostsButton = document.createElement('button');
+    let likedPostsButton = document.createElement('button');
+    myPostsButton.classList.add('active');
+    myPostsButton.id = 'my_posts_button';
+    likedPostsButton.id = 'liked_posts_button';
+    myPostsButton.textContent = 'My Posts';
+    likedPostsButton.textContent = 'Liked Posts'
+
+    // Append post buttons to button container
+    postsButtonContainer.appendChild(myPostsButton);
+    postsButtonContainer.appendChild(likedPostsButton);
+
+    // Add posts card to hold user's recent posts
+    let postsCard = document.createElement('div');
+    postsCard.classList.add('posts-card');
+    postsCard.id = 'posts_card';
+
+    // Add Posts Section            
+    let postsContainer = document.createElement('div');
+    postsContainer.id = 'posts_container';
+    postsCard.appendChild(postsButtonContainer);    
+    postsCard.appendChild(postsContainer);
+    centerCluster.appendChild(postsCard);
+
+    // Add prifile container
+    let profileCard = document.createElement('div');
+    profileCard.id = 'profile_card';
+
+    // Add contents of profile container
+    let profileTitle = document.createElement('h2');
+    let profilePic = document.createElement('img');
+    let bioContainer = document.createElement('div');
+    let bioTitle = document.createElement('h3');
+    let bioText = document.createElement('p');
+    let profileActionContainer = document.createElement('div');
+    let viewProfileButton = document.createElement('button');
+    let messangerButton = document.createElement('button');
+    let logoutButton = document.createElement('button');
+    profileTitle.id = 'profile_title';
+    profilePic.id = 'profile_pic';
+    bioContainer.id = 'bio_container';
+    bioTitle.id = 'profile_bio_title';
+    bioText.id = 'profile_bio_text';
+    profileActionContainer.id = 'profile_action_container';
+    viewProfileButton.id = 'view_profile_button';
+    messangerButton.id ='messanger_button';
+    logoutButton.id = 'profile_logout_button';
+    viewProfileButton.textContent = 'View Profile';
+    messangerButton.textContent = 'Messager';
+    logoutButton.textContent = 'Logout';
+
+    // Append profile components to container
+    bioContainer.appendChild(bioTitle);
+    bioContainer.appendChild(bioText);
+    profileActionContainer.appendChild(viewProfileButton);
+    profileActionContainer.appendChild(messangerButton);
+    profileActionContainer.appendChild(logoutButton);
+    profileCard.appendChild(profileTitle);
+    profileCard.appendChild(profilePic);
+    profileCard.appendChild(bioContainer);
+    profileCard.appendChild(profileActionContainer);
+    rightCluster.appendChild(profileCard);
+    
 }
 
 // window.fetchAllPosts = function(){

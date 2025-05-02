821141807660bdec0209906ca118fc73438e47c6
davodhiambo <jesseomolo@yahoo.com>
2025-05-03 00:15:54 +0300

    feat: add handle like comment.

---
+++ web/static/js/dislike_comment.js
@@ -0,0 +1,46 @@
+import { navigateTo } from "/static/js/routes.js";
+import { renderErrorPage } from "/static/js/error.js";
+
+export function handleDislikeComment(commentID, postID, dislikeContainer) {
+    let formData = new URLSearchParams();
+    formData.append(`comment-id=${commentID}`);
+    formData.append(`post-id=${postID}`);
+
+    fetch('/comments/dislike', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+        },
+        body: formData,
+        credentials: 'include'
+    })
+    .then(response => {
+        if (!response.ok) {
+            if (response.status === 401) {
+                navigateTo('/login');
+                return;
+            } else {
+                renderErrorPage(response.statusText, response.status);
+            }
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.success) {
+            // Update the likes count in the container
+            const countSpan = dislikeContainer.querySelector('#comment_engagement_count');
+            if (countSpan) {
+                countSpan.textContent = data.likesCount;
+            }
+            
+        } else {
+            console.error('Like failed:', data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+    

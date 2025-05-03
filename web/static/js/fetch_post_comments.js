aa849bacecfd152bf61f6e2d8e2e1b82366c6e57
davodhiambo <jesseomolo@yahoo.com>
2025-05-03 03:23:40 +0300

    feat: add fetchComments

---
+++ web/static/js/fetch_post_comments.js
@@ -0,0 +1,31 @@
+export function fetchComments(postCard, postID) {
+    fetch('/comments', {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+    .then(response => {
+        if (!response.ok) {
+            if (response.status === 401) {
+                navigateTo('/login');
+                return;
+            } else {
+                renderErrorPage(response.statusText, response.status);
+            }
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+
+    .then(data => {
+        postCard.innerHTML = '';
+        populateComments(postCard, postID, data.comments);
+    })
+    
+    .catch(error => {
+        console.error('Error fetching post data:', error);
+    });
+}

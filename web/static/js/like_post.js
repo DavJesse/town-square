964bb2aa12f380308525ec18ea68dd825f2a6682
davodhiambo <jesseomolo@yahoo.com>
2025-05-08 15:06:55 +0300

    feat: add updatePostEngagement.

--- web/static/js/like_post.js
+++ web/static/js/like_post.js
@@ -2,7 +2,8 @@ import { navigateTo } from "/static/js/routes.js";
 import { renderErrorPage } from "/static/js/error.js";
 
 export function handleLikePost(postId, likeContainer) {
-    fetch('/posts/like', {
+    let fetchLink = '/posts/like';
+    fetch(fetchLink, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
@@ -43,6 +44,47 @@ export function handleLikePost(postId, likeContainer) {
             console.error('Like failed:', data.message);
         }
     })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+export function updatePostEngagement(fetchLink, likeContainer, dislikeContainer) {
+    fetch(fetchLink, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+        credentials: 'include'
+    })
+
+    .then(response => {
+        if (!response.ok) {
+            if (response.status === 401) {
+                navigateTo('/login');
+                return;
+            } else {
+                renderErrorPage(response.statusText, response.status);
+            }   
+            throw new Error(`Unable To Like Comment`);
+        }
+        return response.json();
+    })
+
+    .then(data => {
+         // Update the like count in the container
+        const likeCountSpan = likeContainer.querySelector('#post_like_count');
+        if (likeCountSpan) {
+            likeCountSpan.textContent = data.like_count;
+        }
+        
+        // Update the dislike count in the container
+        const dislikeCountSpan = dislikeContainer.querySelector('#post_dislike_count');
+        if (dislikeCountSpan) {
+            dislikeCountSpan.textContent = data.dislike_count;
+        }
+    })
+
     .catch(error => {
         console.error('Error:', error);
     });

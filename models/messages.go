a0cd9262bb9d35c4cc9cf349a7f3f65bd94d62e7
toni-od <samaelsamael662@gmail.com>
2025-05-10 12:48:23 +0300

    feat: add websocket handlers

--- models/messages.go
+++ models/messages.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"github.com/gorilla/websocket"
+	"time"
+)
 
 // Message struct to hold message data
 type Message struct {
@@ -10,4 +13,26 @@ type Message struct {
 	Content        string    `json:"content"`
 	Timestamp      time.Time `json:"timestamp"`
 	SenderNickname string    `json:"sender_nickname"`
+}
+
+// WSUser holds a user and their WebSocket connection
+type WSUser struct {
+	User User
+	Conn *websocket.Conn
+}
+
+// Define a struct to hold validation errors.  This will make it easier to
+// send structured error responses.
+type ValidationError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+// Message struct to hold message data for WebSocket communication
+type WSMessage struct {
+	SenderID       int    `json:"sender_id"`
+	ReceiverID     int    `json:"receiver_id"`
+	Content        string `json:"content"`
+	Timestamp      string `json:"timestamp"` // Store as string for easy transfer
+	SenderNickname string `json:"sender_nickname"`
 }

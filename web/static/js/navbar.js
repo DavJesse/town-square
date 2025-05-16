8ec17e304f2141f18f42154050b0982304e068f9
davodhiambo <jesseomolo@yahoo.com>
2025-05-16 15:55:25 +0300

    feat: disable search functionality.

--- web/static/js/navbar.js
+++ web/static/js/navbar.js
@@ -72,40 +72,40 @@ export function renderNavBar() {
   rightCluster.id = 'navbar_right_cluster';
   
   // Create search bar form
-  let searchBar = document.createElement('form');
-  searchBar.method = 'GET';
-  searchBar.action = '/search';
-  searchBar.classList.add('navbar__search-form');
-  searchBar.id = 'search_bar';
+  // let searchBar = document.createElement('form');
+  // searchBar.method = 'GET';
+  // searchBar.action = '/search';
+  // searchBar.classList.add('navbar__search-form');
+  // searchBar.id = 'search_bar';
 
    // Create right navbar cluster container
-   let searchCluster = document.createElement('div');
-   searchCluster.classList.add('search-cluster');
-   searchCluster.id = 'search_cluster';
+  //  let searchCluster = document.createElement('div');
+  //  searchCluster.classList.add('search-cluster');
+  //  searchCluster.id = 'search_cluster';
   
   // Create search bar input field
-  let searchInput = document.createElement('input');
-  searchInput.type = 'text';
-  searchInput.name = 'q';
-  searchInput.id = 'search_input';
-  searchInput.placeholder = 'Search...';    
-  searchInput.classList.add('navbar__search');
-  searchCluster.appendChild(searchInput);
+  // let searchInput = document.createElement('input');
+  // searchInput.type = 'text';
+  // searchInput.name = 'q';
+  // searchInput.id = 'search_input';
+  // searchInput.placeholder = 'Search...';    
+  // searchInput.classList.add('navbar__search');
+  // searchCluster.appendChild(searchInput);
   
   // Create search button
-  let searchButton = document.createElement('button');
-  searchButton.type = 'submit';
-  searchButton.id = 'search_btn';
+  // let searchButton = document.createElement('button');
+  // searchButton.type = 'submit';
+  // searchButton.id = 'search_btn';
   
-  let searchIcon = document.createElement('span');
-  searchIcon.classList.add('material-symbols-outlined');
-  searchIcon.id ='search_icon';
-  searchIcon.textContent = 'search';
+  // let searchIcon = document.createElement('span');
+  // searchIcon.classList.add('material-symbols-outlined');
+  // searchIcon.id ='search_icon';
+  // searchIcon.textContent = 'search';
   
-  searchButton.appendChild(searchIcon);
-  searchCluster.appendChild(searchButton);
-  searchBar.appendChild(searchCluster);
-  rightCluster.appendChild(searchBar);
+  // searchButton.appendChild(searchIcon);
+  // searchCluster.appendChild(searchButton);
+  // searchBar.appendChild(searchCluster);
+  // rightCluster.appendChild(searchBar);
   navBarContainer.appendChild(rightCluster);
 
   // Append nav bar container to the app div
